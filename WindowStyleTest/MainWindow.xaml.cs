2b769cecaa60228a85824ea1a2bbd475422c4e75
Jan Karger <punkerat76@gmail.com>
2014-04-09 15:55:51 +0200

    add test solution (vs2013, .net 4)

---
+++ WindowStyleTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowStyleTest
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+      this.Close();
+    }
+
+    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+    {
+      // quick fix to show the taskbar
+      this.WindowStyle = System.Windows.WindowStyle.ThreeDBorderWindow;
+      this.WindowState = System.Windows.WindowState.Maximized;
+      this.WindowStyle = System.Windows.WindowStyle.None;
+    }
+  }
+}
